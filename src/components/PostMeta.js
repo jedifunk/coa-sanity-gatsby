d17273a6e5dccef4c6206b701a205247d882ce64
Bryce Flory <iam@bryceflory.com>
2022-06-13 13:16:30 +0200

    post meta country link fix

--- src/components/PostMeta.js
+++ src/components/PostMeta.js
@@ -10,7 +10,7 @@ const PostMeta = article => {
       <span className=""> by <span className="author vcard">{article.author && article.author.name}</span></span>
       <div className="cat-links tax-links">
           <ul className="post-categories">
-            {article.country && <li><Link to={article.country.slug.current}>{article.country.name}</Link>, </li>}
+            {article.country && <li><Link to={`/${article.country.slug.current}`}>{article.country.name}</Link>, </li>}
             {article.categories && article.categories.map(cat => (
                 <li key={cat.title}><Link to={`/${cat.slug.current}`}>{cat.title}</Link></li>
             ))
