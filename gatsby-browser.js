e479940b9f28e452db7ce8fdb0157e40e8b113e6
Bryce Flory <iam@bryceflory.com>
2022-06-26 12:33:33 +0200

    post meta edits

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+import '@fontsource/merriweather/300.css'
+import '@fontsource/merriweather/400.css'
+import '@fontsource/merriweather/400-italic.css'
+import '@fontsource/fira-sans/100.css'
+import '@fontsource/fira-sans/200.css'
+import '@fontsource/fira-sans/300.css'
+import '@fontsource/fira-sans/400.css'
+import '@fontsource/fira-sans/500.css'
+import '@fontsource/fira-sans/600.css'
+import '@fontsource/fira-sans/700.css'
+import '@fontsource/fira-sans/900.css'
--- src/components/PostMeta.js
+++ src/components/PostMeta.js
@@ -7,10 +7,9 @@ const PostMeta = article => {
   return (
     <div className="entry-meta">
       <span className="posted-on"><time className="entry-date published updated">{moment(article.publishDate).format(`MMMM D, YYYY`)}</time></span>
-      <span className=""> by <span className="author vcard">{article.author && article.author.name}</span></span>
       <div className="cat-links tax-links">
           <ul className="post-categories">
-            {article.country && <li><Link to={`/${article.country.slug.current}`}>{article.country.name}</Link>, </li>}
+            {article.country && <li><Link to={`/${article.country.slug.current}`}>{article.country.name}</Link></li>}
             {article.categories && article.categories.map(cat => (
                 <li key={cat.title}><Link to={`/${cat.slug.current}`}>{cat.title}</Link></li>
             ))
