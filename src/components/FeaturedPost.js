ce1279f90bcc8bb6367ba0a9e49049e2188f8ec0
Bryce Flory <iam@bryceflory.com>
2022-07-01 16:43:16 +0200

    add image to post entry, adjust featured to two from four

--- src/components/FeaturedPost.js
+++ src/components/FeaturedPost.js
@@ -9,13 +9,15 @@ const FeaturedPost = article => {
     <article className="featured-item">
       <figure>
         <Link to={`/${article.slug.current}`}>
-          {article.featuredImage && <SanityImage {...article.featuredImage} width={300} height={500} />}
+          {article.featuredImage && <SanityImage {...article.featuredImage} width={568} height={320} />}
         </Link>
         <figcaption>
           <h2 className="entry-title">
             <Link to={`/${article.slug.current}`}>{article.title}</Link>
           </h2>
           <PostMeta {...article} />
+
+          <div className="excerpt">{article.excerpt}</div>
         </figcaption>
       </figure>
     </article>
--- src/components/PostEntry.js
+++ src/components/PostEntry.js
@@ -1,16 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
+import SanityImage from 'gatsby-plugin-sanity-image'
 import PostMeta from './PostMeta'
 
 const PostEntry = article => {
-
   return (
     <article className="post-entry" >
       <header className="entry-header">
-          <h2 className="entry-title">
-              <Link to={`/${article.slug.current}`}>{article.title}</Link>
-          </h2>
-          <PostMeta {...article} />
+      <Link to={`/${article.slug.current}`}>{article.featuredImage && <SanityImage {...article.featuredImage} width={347} height={196} />}</Link>
+        <h2 className="entry-title">
+          <Link to={`/${article.slug.current}`}>{article.title}</Link>
+        </h2>
+        <PostMeta {...article} />
       </header>
 
       <div className="excerpt">{article.excerpt}</div>
